0eb0717fed0663cd4ed9782dd83636682f420397
charlexchen <charlexchen@tencent.com>
2023-01-10 14:21:00 +0800

    init: 初始化

--- vite-hm/src/main.ts
+++
@@ -1,6 +0,0 @@
-import './style.css'
-import { renderPage } from './render';
-import { renderStore } from './store';
-
-renderPage();
-renderStore();
---
+++ vite-hmr/src/main.ts
@@ -0,0 +1,6 @@
+import './style.css'
+import { renderPage } from './render';
+import { renderStore } from './store';
+
+renderPage();
+renderStore();
